c8bbc0f029841bc0de370fb153cb2ff7f41bf73e
toumokoroshi <thaybuagtaa0528@gmail.com>
2024-04-16 19:04:19 +0900

    a

---
+++ BootTraining/simulation/header.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/// calcualte dynamics and return dw/dt
+void dynamics(){
+};
+
+/// calcualte kinematics and return dq/dt
+void kinematics(){
+};
+
+/// discrizate functions and calcualte x_n+1 from x_n
+void rk4(){
+};
+#endif
