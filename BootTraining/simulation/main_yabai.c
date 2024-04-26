1aecd2aa3804458487920fe55cfa215ffda5e168
Hyuga <thaybuagtaa0528@gmail.com>
2024-04-26 15:02:11 +0900

    waaaaaaaaaaaaaaaaaaa

--- BootTraining/simulation/main_yabai.c
+++ BootTraining/simulation/main_yabai.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "header_tuyoi.h"
 
-const double dt = 0;
-double I[3][3] = {
-    {0, 0, 0},
-    {0, 0, 0},
-    {0, 0, 0},
-} ;
+void main(){
+    double I[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+    } ;
 
-double k1,k2,k3,k4,q1[4], q2[4], list_q[][5],omega1[3], omega2[3], list_omega[][4],I_inv[3][3], OMEGA[3][3];
-double t_min = 0;
-double t_max = 0;
+    double k1,k2,k3,k4,dt,list_q[][5], list_omega[][4];
 
-inverse_I(**I,**I_inv)
-for ( t = 0; i < count; i++)
-{
-    /* code */
+    double q1[4] ={0};
+    double q2[4]={0};
+    double I_inv[3][3] = {0};
+    double OMEGA[3][3] = {0};
+    double omega[3]={0};
+    double omega_div[3]={0};
+
+    const double t_min = 0;
+    const double t_max = 0;
+    const int N = 0;
+
+    if (N!=0)
+    {
+         double dt = (t_max-t_min)/N;
+    }
+    else
+    {
+        printf("including zero divide at calculating dt");
+        exit(1);
+        return 0;
+    }
+    
+    inverse_I(I,I_inv);
+
+    for ( i = 0; i < N; i++)
+    {
+        dynamics(omega,I,I_inv,omega_div);
+        k1 = dt * omega_div;
+    }
+    
+
 }
---
+++ BootTraining/simulation/test/test1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "C:\lab\BootTraining\simulation\header_tuyoi.h"
+
+void main(){
+    const m = 2;
+    const k = 1;
+    const v0 = 0;
+    const x0 = 5;
+
+    double h = 0.0;
+
+    while (/* condition */)
+    {
+        /* code */
+    }
+    
+}
