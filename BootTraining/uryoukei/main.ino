9a45be4cfcc1b6b711259e30eef8bbeb9e81ab6d
toumokoroshi <thaybuagtaa0528@gmail.com>
2024-05-16 16:22:47 +0900

    lora のinitialize途中

--- BootTraining/uryoukei/main.ino
+++ BootTraining/uryoukei/main.ino
@@ -1,7 +1,6 @@
 #include <LiquidCrystal.h>
 
-/* 
-lcd pin asignments
+/* lcd pin asignments
 esp32 lcdmodule
 28    14(DB7)
 27    13(DB6)
@@ -11,8 +10,7 @@ esp32 lcdmodule
 12     4(RS)
        3(VLC)
        2(Vdd)
-       1(Vss)
-*/
+       1(Vss)*/
 
 #define DB7 28
 #define DB6 27
@@ -22,32 +20,32 @@ esp32 lcdmodule
 #define RS 12
 
 #define LoRa_Rst 9 //リセットピンをアサイン！！！！！！！！！！！！！！！
+//own channel number for lora
+#define CH 1
+//bandwidth, 4 means 125MHz
+#define BW 4
 
-LiquidCrystal lcd(RS, EN, DB4, DB5, DB6, DB7);  //create instance of lcd
+//create instance of lcd
+LiquidCrystal lcd(RS, EN, DB4, DB5, DB6, DB7); 
  
 void setup() {
+//initializing lcd and display "initializing ... " while initializing lora and SDcardslot  
   lcd.begin(16,2);
   lcd.print("Initializing ... ");
 
+//opens serial port for uart with lora
   Serial.begin(115200);
 }
 
 void loop() {
 }
 
-void LoRa_read(){
-  if (Serial.available()>0){
-    /* code */
-  }
-}
-
-void LoRa_write(){
-  
-}
 
 void init_lora(){
+String temp;
+
 // Transition ES920LR to configuration mode
-LoRa_write("config\r\n");
+Serial.write("config\r\n");
 
 // Define the pin to reset the Private LoRa communication module
 pinMode(LoRa_Rst, OUTPUT);
@@ -63,46 +61,46 @@ digitalWrite(LoRa_Rst, HIGH);
 delay(500);
 
 // Receive messages from the Private LoRa communication module
-LoRa_read();
+temp = Serial.readString();
 
 // Select the processor mode of ES920LR
-LoRa_write("processor\r\n");
+Serial.write("processor\r\n");
 delay(500);
 
 // Restore all settings of ES920LR to default
-LoRa_write("load\r\n");
+Serial.write("load\r\n");
 delay(100);
 
 // Set the bandwidth to 125kHz
-LoRa_write("bw 4\r\n");
+Serial.write("bw BW\r\n");
 delay(100);
 
 // Set the spreading factor to 7
-LoRa_write("sf 7\r\n");
+Serial.write("sf 7\r\n");
 delay(100);
 
 // Set the wireless channel number to 1
-LoRa_write("channel 1\r\n");
+Serial.write("channel CH\r\n");
 delay(100);
 
 // Set the PAN network address to ABCD
-LoRa_write("panid ABCD\r\n");
+Serial.write("panid ABCD\r\n");
 delay(100);
 
 // Set the network address of the own node to 500
-LoRa_write("ownid 500\r\n");
+Serial.write("ownid 500\r\n");
 delay(100);
 
 // Set the network address of the destination node to 0000
-LoRa_write("dstid 0000\r\n");
+Serial.write("dstid 0000\r\n");
 delay(100);
 
 // Save the configured settings to the built-in FlashROM
-LoRa_write("save\r\n");
+Serial.write("save\r\n");
 delay(5000);
 
 // Transition ES920LR to operation mode
-LoRa_write("start\r\n");
+Serial.write("start\r\n");
 delay(500);
 }
 
