aa6e6fe615283901d65d7c6675efeddee2940980
Hyuga <thaybuagtaa0528@gmail.com>
2024-05-17 16:10:56 +0900

    weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- BootTraining/uryoukei/test/foo/foo.ino
+++ BootTraining/uryoukei/test/foo/foo.ino
@@ -7,11 +7,18 @@ void setup(){
 void loop(){
     if(Serial.available()>0){
         int incomingByte = Serial.read();
-        Serial.print("with \"print\" : Succeeded to receiving message ");
+        // Serial.print("with \"print\" : Succeeded to receiving message ");
         // Serial.println("with \"println\" : Succeeded to receiving message ");
-        delay(1000);
-        Serial.println(incomingByte, DEC);
+        // delay(1000);
+        // Serial.print("DEC");
+        // Serial.println(incomingByte, DEC);
+        String incomingStrings = Serial.readString();
+        Serial.print("incoming string is :\"");
+        Serial.print(incomingStrings);
+        Serial.print("\"");
+        delay(100);
+        Serial.print("HEX");
         Serial.println(incomingByte, HEX);
-        delay(5000);
+        delay(1000);
     }
 }
