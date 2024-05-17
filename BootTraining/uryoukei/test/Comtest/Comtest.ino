954ff9c1a0b71f229cca655c314ccd54bf6ed9c7
toumokoroshi <thaybuagtaa0528@gmail.com>
2024-05-17 17:47:52 +0900

    tttttttttttttttttttttt

--- BootTraining/uryoukei/test/Comtest/Comtest.ino
+++ BootTraining/uryoukei/test/Comtest/Comtest.ino
@@ -6,34 +6,31 @@
 // spreading factor
 #define SF "7"
 // PANID : the PAN network address in which the node will participate. The devices to be paired must have the same ID
-#define PANID "0001"
+#define PANID "0000"
 // OWNID : the network address of the own(Master Unit) node
-#define OWNID "0001"
+#define OWNID "0000"
 // the network address of the destination(Slave Unit) node
-#define DSID "0000" 
+#define DSID "0001" 
 
 void setup(){
   // opens serial port for uart with lora
   Serial.begin(115200);
 }
 
-void loop()
-{
+void loop(){
 }
 
 // Function to initialize lora
-void init_lora()
-{
-  String temp;
+void init_lora(){
 
   // Transition ES920LR to configuration mode
   Serial.write("config\r\n");
 
   // Define the pin to reset the Private LoRa communication module
   pinMode(LoRa_Rst, OUTPUT);
-  delay(500);
+  delay(100);
   digitalWrite(LoRa_Rst, HIGH);
-  delay(500);
+  delay(100);
 
   // Reset the Private LoRa communication module once
   Serial.println("Reset");
@@ -43,47 +40,82 @@ void init_lora()
   delay(500);
 
   // Receive messages from the Private LoRa communication module
-  temp = Serial.readString();
+  // String response = "";
+  // while (true) {
+  //   if (Serial.available()>0) {
+  //       continue;
+  //     }
+  //   break;
+  //   }
+  //   delay(10);
 
+  while (Serial.available()>0) {
+      char c = Serial.read();
+      if (response.endsWith("\r\n")) {
+        break;
+    }
+  }
+  delay(10);
+  
   // Select the processor mode of ES920LR
   Serial.write("processor\r\n");
-  delay(500);
+  waitForOK();
 
   // Restore all settings of ES920LR to default
   Serial.write("load\r\n");
-  delay(100);
+  waitForOK();
 
   // Set the bandwidth
   Serial.write("bw %s\r\n", BW);
-  delay(100);
+  waitForOK();
 
   // Set the spreading factor
   Serial.write("sf %s\r\n", sf);
-  delay(100);
+  waitForOK();
 
   // Set the wireless channel number
   Serial.write("channel %s\r\n", CH);
-  delay(100);
+  waitForOK();
 
   // Set the PAN network address
   Serial.write("panid %s\r\n", PANID);
-  delay(100);
+  waitForOK();
 
   // Set the network address of the own node
   Serial.write("ownid %s\r\n", OWNID);
-  delay(100);
+  waitForOK();
 
   // Set the network address of the destination node to 0000
   Serial.write("dstid %s\r\n", DSID);
-  delay(100);
+  waitForOK();
 
   // Save the configured settings to the built-in FlashROM
   Serial.write("save\r\n");
-  delay(5000);
+  waitForOK();
 
   // Transition ES920LR to operation mode
   Serial.write("start\r\n");
-  delay(100);
+  waitForOK();
+}
+
+// Function to wait for "OK\r\n" response from lora
+bool waitForOK() {
+// void waitForOK() {
+  String response = "";
+  while (true) {
+    if (Serial.available()) 
+    {
+      char c = Serial.read();
+      response += c;
+      if (response.endsWith("OK\r\n")) 
+      {
+        return true;
+        // break;
+      }
+    }
+    delay(10);
+  }
+  return false;
 }
 
 // Function to divide data by comma and store them in an array
@@ -104,6 +136,5 @@ int splitData(String data, String *dataArray, int maxArraySize)
   {
     dataArray[dataCount++] = data.substring(startIndex);
   }
-
   return dataCount;
 }
--- BootTraining/uryoukei/test/foo/foo.ino
+++ BootTraining/uryoukei/test/foo/foo.ino
@@ -1,3 +1,6 @@
+const int maxArraySize = 10; // 配列の最大サイズを定義
+String dataArray[maxArraySize]; // データを格納する配列
+
 void setup(){
     Serial.begin(115200);
     Serial.println("hello world");
@@ -5,20 +8,39 @@ void setup(){
 }
 
 void loop(){
-    if(Serial.available()>0){
-        int incomingByte = Serial.read();
-        // Serial.print("with \"print\" : Succeeded to receiving message ");
-        // Serial.println("with \"println\" : Succeeded to receiving message ");
-        // delay(1000);
-        // Serial.print("DEC");
-        // Serial.println(incomingByte, DEC);
-        String incomingStrings = Serial.readString();
-        Serial.print("incoming string is :\"");
-        Serial.print(incomingStrings);
-        Serial.print("\"");
-        delay(100);
-        Serial.print("HEX");
-        Serial.println(incomingByte, HEX);
-        delay(1000);
+    String receivedData = "";
+    while (Serial.available())
+    {
+        char buff = Serial.read();
+        receivedData += buff;
     }
+    int CRLFindex = receivedData.indexOf("\\");
+    receivedData.remove(CRLFindex);
+    int dataCount = splitData(receivedData, dataArray, maxArraySize);
+    
+    // データを配列に格納した後、各データを表示
+    for (int i = 0; i < dataCount; i++) {
+      Serial.println(dataArray[i]);
+    }
+  }
+
+// Function to divide data by comma and store them in an array
+int splitData(String data, String *dataArray, int maxArraySize)
+{
+  int dataCount = 0;
+  int startIndex = 0;
+  int endIndex = data.indexOf(',');
+
+  while (endIndex != -1 && dataCount < maxArraySize)
+  {
+    dataArray[dataCount++] = data.substring(startIndex, endIndex);
+    startIndex = endIndex + 1;
+    endIndex = data.indexOf(',', startIndex);
+  }
+
+  if (dataCount < maxArraySize)
+  {
+    dataArray[dataCount++] = data.substring(startIndex);
+  }
+  return dataCount;
 }
