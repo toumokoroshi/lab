f1e7474cd6e0d442fb6dce9a4925816cda740bf6
toumokoroshi <thaybuagtaa0528@gmail.com>
2024-05-23 14:37:19 +0900

    qqqqqqqqqqqqqq

--- BootTraining/uryoukei/test/waaaaaaaa/foo.c
+++ BootTraining/uryoukei/test/waaaaaaaa/foo.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+struct sample
+{
+    short counter = 1;
+    short anomaryID = 2;    /* アノマリID */
+    float SPVoltage = 1.00;   /* 太陽光パネル電圧値 */
+    float battVoltage = 2.00; /* 二次電池電圧 */
+    float current = 1.00;     /* 電流 */
+    float rainfall = 1.00;    /* 雨量 */
+    short rain = 1;        /* 雨検知 */
+    short pressure = 1013;    /* 気圧 */
+    float temp = 20.0;     /* 気温 */
+    float humidity = 15.5;    /* 湿度 */
+} ;
 void main(){
-    float a = 100;    
-    printf("a = %f\n",a);
-    a /= 30.0;
-    printf("a/10 = %0.3f\n",a);
-    }
+    char* h[5]
+}
 
