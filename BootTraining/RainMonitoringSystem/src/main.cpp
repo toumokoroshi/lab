68be0ada5798f44b983186ffb8b8f86a7399301e
Hyuga <thaybuagtaa0528@gmail.com>
2024-05-29 16:39:09 +0900

    l

--- BootTraining/RainMonitoringSystem/src/main.cpp
+++ BootTraining/RainMonitoringSystem/src/main.cpp
@@ -5,11 +5,16 @@
 #include <raindetecter_handler.hpp>
 #include <SDmodule.hpp>
 
-float pre_wd_voltage = 0.0;//previous value of water detector volage value
+#define RX_PIN 22
+#define TX_PIN 23
+#define SWITCH_PIN 7
+#define BAUDRATE 9600
+
+float pre_wd_voltage = 0.0; // previous value of water detector volage value
 int wd_decrease_counter = 0;
 
 LiquidCrystal lcd(RS, EN, DB4, DB5, DB6, DB7);
-byte celsius_degree[8]={   // bit data for celsius degree character
+byte celsius_degree[8] = {   // bit data for celsius degree character
     B11100,
     B10100,
     B11100,
@@ -19,121 +24,115 @@ byte celsius_degree[8]={   // bit data for celsius degree character
     B01111,
 };
 
-formattedData datastruct;  
+char rainfallBuff[8];
+char tempBuff[8];
+char preassureBuff[8];
+char humidityBuff[8];
+char counterBuff[8];
+char anomaryIDBuff[8];
+char pvBuff[8];
+char battBuff[8];
+char currentBuff[8];
+
+formattedData datastruct;
 char receieved_data[44];
 
 bool displayData_weather = true; // true: display weather data, false: display system data
 
 // Interrupt flag
 volatile bool dataReceived = false;
+String strBuff;
+static bool conbertchecker = 0;
 
 // Interrupt handler for serial data reception
 void IRAM_ATTR serialEvent1() {
-    strcpy(receieved_data,"");
-    int i = 0;
-    while (Serial.available()) {
-        char buff = Serial.read();
-        receieved_data[i] = buff;
-        i++;
-        if (buff == '\n') {
+    while (Serial1.available()) {
+        char inChar = (char)Serial1.read();
+        strBuff += inChar;
+        if (inChar == '\n') {
             dataReceived = true;
-        } 
+            break; // Exit the loop once a complete message is received
+        }
     }
 }
 
 // Function to check the state of the switch
 void checkSwitch() {
     static bool lastSwitchState = HIGH;
+    static unsigned long lastDebounceTime = 0;
+    unsigned long debounceDelay = 50; // 50ms debounce delay
     bool currentSwitchState = digitalRead(SWITCH_PIN);
-  if (lastSwitchState  ^ currentSwitchState ) {
-    displayData_weather = currentSwitchState;
-    delay(200); // Debounce delay
-  }
-}
 
-void setup()
-{
-    lcd.begin(16, 2);
-    lcd.print("Initializing...");
-    lcd.createChar(0,celsius_degree);
-
-    // starting Serial com with lora
-    Serial.begin(BAUDRATE);
-    while (!Serial);
- 
-    // Set pin mode for the switch
-    pinMode(SWITCH_PIN, INPUT_PULLUP);
-    
-    // starting com with and initializing SDmod
-    if(!SD.begin()){
-        lcd.print("Card Mount Failed");
-        delay(2000);
-        return;
+    if (currentSwitchState != lastSwitchState) {
+        lastDebounceTime = millis();
     }
 
-    lcd.clear();
-    delay(50);
-    lcd.print("Get Ready!");
-    delay(1000);
-    lcd.print("Waiting for data");
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+        if (currentSwitchState != displayData_weather) {
+            displayData_weather = currentSwitchState;
+        }
+    }
 
-    // Set up interrupt
-    attachInterrupt(digitalPinToInterrupt(3), serialEvent1, CHANGE);
+    lastSwitchState = currentSwitchState;
 }
 
-void loop()
-{
-    // Check if an interrupt has occurred
-    noInterrupts(); // Disable interrupts while checking and resetting the flag
+void setup() {
+    lcd.begin(16, 2);
+    lcd.print("Initializing...");
+    lcd.createChar(0, celsius_degree);
+
+    Serial1.begin(BAUDRATE, SERIAL_8N1, RX_PIN, TX_PIN);
+    while (!Serial1);
+
+    pinMode(SWITCH_PIN, INPUT);
+
+    lcd.clear();
+    lcd.print("Get Ready!");
+    delay(1000);
+    lcd.clear();
+    lcd.print("Waiting for data");
+
+    attachInterrupt(digitalPinToInterrupt(RX_PIN), serialEvent1, CHANGE);
+}
+
+void loop() {
+    noInterrupts();
     bool received = dataReceived;
     dataReceived = false;
-    interrupts(); // Re-enable interrupts
+    interrupts();
 
     if (received) {
-        writeFile(SD, "/data.txt", receieved_data); 
-        formatData(receieved_data, &datastruct);
-        isRainingorNot(&datastruct.is_raining,&datastruct.waterdetector_Voltage,&pre_wd_voltage,&wd_decrease_counter);
+        if (conbertchecker) {
+            strBuff.toCharArray(receieved_data, sizeof(receieved_data));
+            formatData(receieved_data, &datastruct);
+            isRainingorNot(&datastruct.is_raining, &datastruct.waterdetector_Voltage, &pre_wd_voltage, &wd_decrease_counter);
 
-        //conbert each data to characters to display
-        char rainfallBuff[8];
-        char tempBuff[8];
-        char preassureBuff[8];
-        char humidityBuff[8];
-        snprintf(rainfallBuff, sizeof(rainfallBuff), "%4.1f", datastruct.rainfall);
-        snprintf(tempBuff, sizeof(tempBuff), "%4.1f", datastruct.temp);
-        snprintf(preassureBuff, sizeof(preassureBuff), "%d", datastruct.pressure);
-        snprintf(humidityBuff, sizeof(humidityBuff), "%3.1f", datastruct.humidity);
+            snprintf(rainfallBuff, sizeof(rainfallBuff), "%4.1f", datastruct.rainfall);
+            snprintf(tempBuff, sizeof(tempBuff), "%4.1f", datastruct.temp);
+            snprintf(preassureBuff, sizeof(preassureBuff), "%d", datastruct.pressure);
+            snprintf(humidityBuff, sizeof(humidityBuff), "%3.1f", datastruct.humidity);
+            snprintf(counterBuff, sizeof(counterBuff), "%d", datastruct.counter);
+            snprintf(anomaryIDBuff, sizeof(anomaryIDBuff), "%d", datastruct.anomaryID);
+            snprintf(pvBuff, sizeof(pvBuff), "%3.1f", datastruct.PV_Voltage);
+            snprintf(battBuff, sizeof(battBuff), "%3.1f", datastruct.batt_Voltage);
+            snprintf(currentBuff, sizeof(currentBuff), "%4.1f", datastruct.current);
+            conbertchecker = 0;
+        }
 
-        char counterBuff[8];
-        char anomaryIDBuff[8];
-        char pvBuff[8];
-        char battBuff[8];
-        char currentBuff[8];
-        snprintf(counterBuff, sizeof(counterBuff), "%d", datastruct.counter);
-        snprintf(anomaryIDBuff, sizeof(anomaryIDBuff), "%d", datastruct.anomaryID);
-        snprintf(pvBuff, sizeof(pvBuff), "%4d", datastruct.PV_Voltage);
-        snprintf(battBuff, sizeof(battBuff), "%3.1f", datastruct.batt_Voltage);
-        snprintf(currentBuff, sizeof(currentBuff), "%4.1f", datastruct.current);
-        
-        // Clear the LCD and display new data
         lcd.clear();
         lcd.setCursor(0, 0);
 
         if (displayData_weather) {
             static bool datablink = 0;
-            if (datastruct.is_raining)
-            {
-                lcd.print("Rainfall:     mm"); // ５マス分のスペースは降水量の５文字分
+            if (datastruct.is_raining) {
+                lcd.print("Rainfall:     mm");
                 lcd.setCursor(9, 0);
                 lcd.print(rainfallBuff);
             } else {
                 lcd.print("No rain");
             }
 
-            if (datablink)
-            {
-                lcd.setCursor(0, 1);
-                lcd.print("                ");
+            if (datablink) {
                 lcd.setCursor(0, 1);
                 lcd.print(tempBuff);
                 lcd.setCursor(4, 1);
@@ -144,13 +143,11 @@ void loop()
                 lcd.print("%");
             } else {
                 lcd.setCursor(0, 1);
-                lcd.print("                ");
-                lcd.setCursor(0, 1);
                 lcd.print(preassureBuff);
                 lcd.setCursor(5, 1);
                 lcd.print("hpa");
             }
-                datablink = !datablink;
+            datablink = !datablink;
         } else {
             lcd.setCursor(0, 0);
             lcd.print("PV/Bat:    /");
@@ -169,7 +166,8 @@ void loop()
             lcd.setCursor(14, 1);
             lcd.print(anomaryIDBuff);
         }
-        delay(2000);
-        checkSwitch();
     }
+
+    delay(2000);
+    checkSwitch();
 }
