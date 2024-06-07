e85a606565a9ba810cd0870f408fa514494f96b1
toumokoroshi <thaybuagtaa0528@gmail.com>
2024-06-07 12:05:52 +0900

    d

--- BootTraining/raindetectionsystem/lib/SDmodule/SDmodule.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SDMODULE_HPP
-#define SDMODULE_HPP
-#include <Arduino.h>
-#include <FS.h>
-#include <SPI.h>
-
-bool writeFile(fs::FS &fs, const char *path, const char *message);
-bool createFile(fs::FS &fs, const char *path);
-#endif
--- BootTraining/uryoukei/README.md
+++
@@ -1,2 +0,0 @@
-add the path of "ArduinoOutput" folder to veryfy codes
-The name of the Arduino sketch folder and the name of the main sketch file must match.
